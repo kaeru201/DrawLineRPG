06ecdfc2af3b128ae30f1b4e84f4bc3237c1ed69
DESKTOP-I3PECJ7\kawakupo <ryomaryoma123456@gmail.com>
2025-08-28 00:55:33 +0900

    矢印追加

---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System;
+
+public class TurnManager : MonoBehaviour
+{
+   public static TurnManager instance;
+
+    public bool PlayerTurn = false;
+
+
+   
+    
+    
+    
+    
+    
+    
+   
+}
