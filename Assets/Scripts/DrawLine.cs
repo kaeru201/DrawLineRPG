7c6f9f53c5a3ba2b6bf3c14906e6c1e97b052cec
DESKTOP-I3PECJ7\kawakupo <ryomaryoma123456@gmail.com>
2025-09-28 16:41:32 +0900

    pointにアタッチする直前

--- Assets/Scripts/DrawLine.cs
+++ Assets/Scripts/DrawLine.cs
@@ -12,12 +12,13 @@ public class DrawLine : MonoBehaviour
     CircleCollider2D circleCollider;
     Transform startPosition;
 
-    bool isTrigerPoint = false;//プレバフ化した攻撃Bollでつける変数を一旦つけてる
+   // bool isTrigerPoint = false;//プレバフ化した攻撃Bollでつける変数を一旦つけてる
     float maxLineRange;//本当はskillから参照してくる変数
     int posCount;//
-    bool isDraw = false;
+    bool isDrawing = false;
     bool ready;
     float currentLineRange;//現在の書ける長さ
+    Vector2 lastAddPoint;//最後に追加した点を記憶しておく変数
 
 
 
@@ -31,7 +32,7 @@ public class DrawLine : MonoBehaviour
         startPosition = GetComponent<Transform>();
 
         lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f; 
 
     }
 
@@ -39,7 +40,7 @@ public class DrawLine : MonoBehaviour
     void Update()
     {
         //線を書くターンではないのなら何もしない
-        if (battleManager.State != BattleState.DrawLinTurn) return;
+       // if (battleManager.State != BattleState.DrawLinTurn) return;
 
         //マウスのスクリーン座標をワールド座標に変換する
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
@@ -50,7 +51,7 @@ public class DrawLine : MonoBehaviour
             if (circleCollider.bounds.Contains(mousePos)) return;
 
 
-            isDraw = true;
+            isDrawing = true;
             //初期化
             posCount = 0;
             lineRenderer.positionCount = 0;
@@ -61,16 +62,44 @@ public class DrawLine : MonoBehaviour
         }
 
         //左クリック押し続けている間かつisDrawがtrueなら
-        if (Input.GetMouseButton(0) && isDraw)
+        if (Input.GetMouseButton(0) && isDrawing)
         {
+            if(currentLineRange >= maxLineRange)
+                {
+
+                isDrawing = false;
+                return;
+            }
+
+            //最後点と今のマウスの位置の差
+            float distanceMouse = Vector2.Distance(lastAddPoint, mousePos);
+
+            //もししきい値よりマウスが離れたら
+            if (distanceMouse > 0.1f)
+            { 
+            　　　//ベクトル
+               Vector2 direction = (mousePos - lastAddPoint).normalized;
+
+                //
+                Vector2 newPoint = lastAddPoint + direction * 0.1f;
+
+                //
+                float lengthToAdd = Vector2.Distance(lastAddPoint, newPoint);
+
+
+                //if(currentLineRange + lengthToAdd > maxLineRange){}
+                
+                AddLine(newPoint);
+                currentLineRange += lengthToAdd;
+            }
 
 
         }
 
         //左クリックを離した時かつREADYがfalseなら
-        if(Input.GetMouseButtonUp(0)) 
+        if (Input.GetMouseButtonUp(0) && !ready) 
         {
-            isDraw = false;
+            isDrawing = false;
             lineRenderer.positionCount = 0;//やりなおし
         }
 
@@ -78,7 +107,7 @@ public class DrawLine : MonoBehaviour
         if(Input.GetMouseButtonDown(1))
         {
             ready = true;
-            isDraw=false;
+            isDrawing=false;
             //次のキャラSkillに移動
             //いないならREADYのUIを出して行けるなら敵の線を引くターンに移動
         }
@@ -87,6 +116,7 @@ public class DrawLine : MonoBehaviour
 
     }
 
+    //線を引くメソッド(点)
     void AddLine(Vector2 point)
     {
         lineRenderer.positionCount = posCount + 1;//繰り返す度に点を増やしていく
@@ -96,13 +126,13 @@ public class DrawLine : MonoBehaviour
 
 
     //一旦ここに書いてるけど、プレハブの線を出すスクリプトに書く内容かも　あとタグで探したくない
-    void OnCollisionEnter(Collision collision)
-    {
-        //自分以外のPointに触れたら　応急処置で始点をPointからずらす
-        if (collision.gameObject.CompareTag("Point"))
-        {
-            isTrigerPoint = true;
-        }
-    }
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    //自分以外のPointに触れたら　応急処置で始点をPointからずらす
+    //    if (collision.gameObject.CompareTag("Point"))
+    //    {
+    //        isTrigerPoint = true;
+    //    }
+    //}
 
 }
