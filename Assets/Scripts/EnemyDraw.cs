2ac943404e3315d2062dde1f5539208c33114e7f
DESKTOP-I3PECJ7\kawakupo <ryomaryoma123456@gmail.com>
2025-10-09 23:41:13 +0900

    EnemyDraw調整

--- Assets/Scripts/EnemyDraw.cs
+++ Assets/Scripts/EnemyDraw.cs
@@ -8,15 +8,13 @@ public class EnemyDraw : MonoBehaviour
     LineRenderer lineRenderer;
     Transform startPosition;
 
-    [SerializeField] Transform Player1Point;
-    [SerializeField] Transform Player2Point;
-    [SerializeField] Transform Player3Point;
-
     [SerializeField] Transform Enemy1Point; //絶対自分のTrasformを取得することになるから2回取得することになって
     [SerializeField] Transform Enemy2Point;//違う気がする
     [SerializeField] Transform Enemy3Point;
 
+    [SerializeField] Transform[] PlayerPoint = new Transform[3];
 
+    //Transform[] test = new Transform[3];
 
     void Start()
     {
@@ -26,16 +24,34 @@ public class EnemyDraw : MonoBehaviour
         lineRenderer.startWidth = 0.1f;
         lineRenderer.endWidth = 0.1f;
 
-       
+
     }
 
-   
+    //線を一気に描くのではなく、プレイヤーのように徐々に描いてほしい
+    //DrawLineと違って最終目的地がPlayerPointで決まっている
     void Update()
     {
-        if (battleSystem.CurrentBState == BattleState.EnemyTurn)
-        {
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, Player1Point.position);
-        }
+        if (battleSystem.CurrentBState != BattleState.EnemyTurn) return;
+
+        Transform y = RandomPoint();
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, y.position);
+        battleSystem.CurrentBState = BattleState.BattleTurn;//これだと一人しかかけないから用調整
+
     }
+
+    Transform RandomPoint()//ｘ、ｙを適切な名前に治す
+    {
+        int x = Random.Range(0, PlayerPoint.Length);
+        Transform Point = PlayerPoint[x];
+        return Point;
+    }
+
+    //Transform Test()
+    //{
+    //    int length = Random.Range(0, test.Length);
+    //    Transform ttest = test[length];
+    //    return ttest;
+    //}
+
 }
