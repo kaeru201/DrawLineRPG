c44efabd45d194be740206693049fa4f2c74d6ca
DESKTOP-I3PECJ7\kawakupo <ryomaryoma123456@gmail.com>
2025-10-18 13:57:35 +0900

    ダイヤログメソッド完成

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -1,91 +1,59 @@
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 //Dialogにアタッチするスクリプト　ダイヤログで流すログを管理をする
 //Listをテキストが発生するたびに追加してテキストを管理
 public class Dialog : MonoBehaviour
 {
-    //[SerializeField] List<GameObject> diaLogGameObj;
-    //[SerializeField] List<TextMeshProUGUI> diaLogText;
-    [SerializeField] TextMeshProUGUI prefabText ;
+    
+    [SerializeField] TextMeshProUGUI prefabText;
 
-   public  List<TextMeshProUGUI> dialogTexts = new List<TextMeshProUGUI>();
+    public List<TextMeshProUGUI> dialogTextObjs = new List<TextMeshProUGUI>();
 
-    Vector3 startPosition = new Vector3(0, 270, 0);
-    Vector3 nextPosition = new Vector3();
-
-    public int currentCount;
-
-    void Start()
-    {
-        //nextPosition = startPosition;
-
-        // nextPoint = startPoint;//これだと2週目以降がうまくいかないかも　newPointの位置がずれて
-        AddDialog("test");
-        
-    }
-
-    private void Awake()
-    {
-        AddDialog("test2");
-
-    }
-
-    void Update()
-    {
-        if(currentCount != dialogTexts.Count)
-        {
-            //順番に上から表示
-            for (int i = 0; i < dialogTexts.Count; i++)
-            {
-                TextMeshProUGUI text = Instantiate(dialogTexts[i], transform.parent);//変数text使いまわせるのか?
-                text.transform.SetParent(transform);
-                text.transform.localPosition = nextPosition;
-                text.transform.localScale = new Vector3(1, 1, 1);
-                text.text = "test0";
-                nextPosition.y -= 100;
-
-                Debug.Log("6");
-
-                currentCount++;
-            }
-        }
-       
-    }
-
+    Vector3 startPosition = new Vector3(0, 260, 0);//初期地点
+    Vector3 nextPosition = new Vector3();//次の出現地点
 
 
     //ダイヤログで表示するものが出るたびに他のクラスから呼ばれて、ダイヤログを更新するメソッド
     public void AddDialog(string dialog)
     {
-        //テキストが送られてくるたびにListにprefabを追加
-       
+      
+        //まだListに要素がないなら次の出現地点を初期地点に
+        if (dialogTextObjs.Count == 0) nextPosition = startPosition;
 
-        if (dialogTexts.Count == 0) nextPosition = startPosition;
-        
-        //dialogTextの要素数が6以下なら
-        if (dialogTexts.Count <= 6)
+        //dialogTextの要素数が6以上だったら
+        if (dialogTextObjs.Count >= 6)
         {
-            dialogTexts.Add(prefabText);
+            //要素数が1番上のオブジェクトごとテキストとListを削除して           
+            Destroy(dialogTextObjs[0].gameObject);
+            dialogTextObjs.RemoveAt(0);
 
-            
-           
+            //残っているすべてListのオブジェクトを上にずらす
+            foreach (TextMeshProUGUI textObj in dialogTextObjs)
+            {
+                Vector3 textPos = textObj.gameObject.transform.localPosition;
 
+                textPos.y += 100;
 
-            //TextMeshProUGUI text1 = Instantiate(prefabDialogText[0], new Vector3(0, 270, 0), Quaternion.identity);
-            //text1.text = "player120ダメージ";
+                textObj.gameObject.transform.localPosition = textPos;
+            }
+
+            nextPosition.y += 100f;//次の出現地点を上にずらす(枠から出ないように)
 
         }
-        //dialogTextの要素数が7以上だったら
-        else
-        {
-            //要素数が1番上のテキストを削除して
 
-            //全員位置を上にずらして一番したに7番目以降の要素を表示 ←これは上の6個いかに任せてここでは削除するのに専念してもいいかも
-            //多分処理が一気にきて消しきれないとか出てくる
-        }
+        //ダイヤログテキストをインスタンス化
+        TextMeshProUGUI text = Instantiate(prefabText);
+        dialogTextObjs.Add(text);//dialogTextリストに追加
+        text.transform.SetParent(transform);//親をこのオブジェクトに
+        text.transform.localPosition = nextPosition;//出現地点
+        text.transform.localScale = new Vector3(1, 1, 1);
+        text.text = dialog;//テキストをメソッドの引数に変更
+
+        nextPosition.y -= 100;//次の出現地点を下にずらす
+
     }
-
 
 }
