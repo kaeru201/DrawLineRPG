83f70ce247ce0017278cebcec6e339ae7caa71f3
DESKTOP-I3PECJ7\kawakupo <ryomaryoma123456@gmail.com>
2025-10-20 23:49:41 +0900

    UnitTeam.cs追加

---
+++ Assets/Scripts/GameScripts/UnitTeam.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+//インスペクター上で離れたunitにそれぞれのUnitを入れる
+public class UnitTeam : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
